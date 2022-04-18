8fafc54063679cdbdb59b359e6935702efac4b80
Klopolupka007 <pankrukhin.maxim@gmail.com>
2022-04-18 18:32:14 +0300

    Закончил оформлять СО, совместил две системы в одну - в канон виде

--- src/SystemPanel.java
+++ src/SystemPanel.java
@@ -23,14 +23,11 @@ public class SystemPanel extends JPanel {
 
     //Функция для заголовков
     void LabelCreate(){
-        String [] bd = {"Система ограничений:", "СО в каноническом виде:"};
-        JLabel[] labels = new JLabel[2];
-        for (int i=0; i<2; i++){
-            labels[i] = new JLabel(bd[i]);
-            labels[i].setFont(ColorFont.titles);
-            labels[i].setBounds(150+(i*400), 10,400, 20);
-            add(labels[i]);
-        }
+        JLabel labels = new JLabel();
+            labels = new JLabel("Система ограничений в каноническом виде:");
+            labels.setFont(ColorFont.titles);
+            labels.setBounds(150, 10,500, 20);
+            add(labels);
     }
 
     //Функция для оформления системы ограничений в обычном и каноническом виде.
@@ -39,24 +36,39 @@ public class SystemPanel extends JPanel {
         //labelX - надписи переменных x, которые итерируются с 0 до количества переменных x
         list = new Double[constr+1][count+1];
 
-        JTextField[][] text = new JTextField[constr+1][count+1];
+        JTextField[][] text = new JTextField[constr][count+1];
         JLabel[][] labelX = new JLabel[constr][count];
 
+        FieldsListen listen = new FieldsListen();
         /*
-        labelX[count] = new JLabel(CreateString1(count, constr));
+        labelX[count] = new JLabel(FieldsListen.CreateString1(count, constr));
         labelX[count].setFont(ColorFont.simple);
         labelX[count].setBounds(215, 53, 400,20);
         add(labelX[count]);
         */
 
 
+
         //Создаем и добавляем компоненты текстовых полей в систему:
         for (int i =0; i<constr; i++) {
-            text[i][constr] = new JTextField();
-            text[i][constr].setHorizontalAlignment(JTextField.CENTER);
-            text[i][constr].setFont(ColorFont.simple);
-            text[i][constr].setBounds(30 + (count * 80), 40 + (i * 40), 40, 20);
-            add(text[i][constr]);
+            text[i][count] = new JTextField();
+            text[i][count].setHorizontalAlignment(JTextField.CENTER);
+            text[i][count].setFont(ColorFont.simple);
+            text[i][count].setBounds(55 + (count * 80), 40 + (i * 40), 40, 20);
+            add(text[i][count]);
+            listen.finalI = i;
+            listen.finalJ = count;
+
+            listen.count = count;
+            listen.constr = constr;
+
+            listen.text = text;
+            listen.list = list;
+
+            text[i][count].addActionListener(listen);
+
+
+
         }
         for (int i=0;i<constr; i++){
             for (int j = 0; j<count; j++ ){
@@ -65,50 +77,39 @@ public class SystemPanel extends JPanel {
                 text[i][j].setFont(ColorFont.simple);
                 text[i][j].setBounds(20+(j*80), 40+(i*40), 40,20);
 
-                if (j == count-1) labelX[i][j] = new JLabel(CreateString(j, true));
-                else labelX[i][j] = new JLabel(CreateString(j, false));
+                if (j == count-1) labelX[i][j] = new JLabel(CreateString(j, true, count+1+i));
+                else labelX[i][j] = new JLabel(CreateString(j, false, -1));
                 labelX[i][j].setFont(ColorFont.simple);
                 labelX[i][j].setBounds(62+(j*80), 40+(i*40), 100,20);
 
                 add(labelX[i][j]); add(text[i][j]);
 
                 //Listener для обработки ввода в ячейки поля ввода и еще некоторых других функциях (описаны ниже)
-                int finalI = i;
-                int finalJ = j;
-                text[i][j].addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        list[finalI][finalJ] = Double.valueOf(String.valueOf(text[finalI][finalJ]));
-                        labelX[finalI][finalJ].setText(CreateString1(count, constr));
+                listen.finalI = i;
+                listen.finalJ = j;
 
-                        /*
-                        if (e.getSource()==text[text.length-1]){
-                            //Переводим фокус на нулевой индекс
-                            e.setSource(text[0]); ((JTextField) e.getSource()).requestFocusInWindow();
-                        } else {
-                            //Переводим фокус левее
-                            e.setSource(text[Arrays.asList(text).indexOf((JTextField)e.getSource()) + 1]);
-                            ((JTextField) e.getSource()).requestFocusInWindow();
-                        }
-                        */
-                    }
-                });
+                listen.count = count;
+                listen.constr = constr;
 
+                listen.labelX = labelX;
+                listen.text = text;
+                listen.list = list;
+                text[i][j].addActionListener(listen);
             }
         }
 
     }
 
     //Функция оформления строки ограничения в системе ограничений
-    String CreateString(int i, boolean j){
+    String CreateString(int i, boolean j, int constr){
         i+=1;
         if (j){
-            return "*x"+i+" <= ";
+            return "*x" + i +" + x" + constr + " = ";
         }
-        else return "*x"+i+" + ";
+        else return "*x" + i + " + ";
     }
-
     //Функция оформления строки ограничений в системе ограничений в канон виде
+ /*
     String CreateString1(int count, int constr){
         StringBuilder temp = new StringBuilder();
 
@@ -120,10 +121,36 @@ public class SystemPanel extends JPanel {
         }
         for (int i=count; i<constr+count; i++){
             if (i!=constr-1+count) temp.append("x").append(i + 1).append(" + ");
-            else temp.append("x").append(i + 1).append(" =");
+            else temp.append("x").append(i + 1).append(" = ");
         }
         return String.valueOf(temp);
     }
+*/
+}
+class FieldsListen implements ActionListener{
+    int finalI, finalJ, count, constr;
+    JTextField[][] text;
+    Double[][] list;
+    JLabel[][] labelX;
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (!text[finalI][finalJ].getText().equals(""))
+            list[finalI][finalJ] = Double.valueOf(text[finalI][finalJ].getText());
+        //labelX[finalI][finalJ].setText(CreateString1(count, constr));
+
+        if (finalI+1!=constr) {
+            if (finalJ+1!=count+1) {
+                e.setSource(text[finalI][finalJ + 1]);
+                ((JTextField) e.getSource()).requestFocusInWindow();
+            } else {
+                e.setSource(text[finalI + 1][0]);
+                ((JTextField) e.getSource()).requestFocusInWindow();
+            }
+        }
+        else {
+            e.setSource(text[0][0]); ((JTextField) e.getSource()).requestFocusInWindow();
+        }
+    }
 }
 
